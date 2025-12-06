6073800d5fded74c47dc17eaae721a8a68fced11
emoit akelo <emoitakelo@gmail.com>
2025-12-06 08:15:45 +0300

    live

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Always calculate the date using Kenya timezone
-  const today = new Date().toLocaleDateString("en-CA", {
-    timeZone: "Africa/Nairobi",
-  });
+  // Convert to YYYY-MM-DD
+  const today = new Date().toISOString().split("T")[0];
 
   // Redirect to the daily predictions page
   redirect(`/predictions/${today}`);
