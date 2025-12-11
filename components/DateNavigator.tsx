948444172aba3c8f8be2b7cf28950a2f59673d0b
emoit akelo <emoitakelo@gmail.com>
2025-12-11 16:43:40 +0300

    date

--- components/DateNavigator.tsx
+++ components/DateNavigator.tsx
@@ -61,7 +61,7 @@ export default function DateNavigator({ date }: Props) {
           {"<"}
         </button>
 
-        <div className="text-xs text-gray-400 whitespace-nowrap">
+        <div className="text-xs font-semibold text-gray-400 whitespace-nowrap">
           {formattedDate}
         </div>
 
