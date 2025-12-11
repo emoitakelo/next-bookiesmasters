4598d64c0605c9135e0bf309b9d0ce671b04423b
emoit akelo <emoitakelo@gmail.com>
2025-12-11 10:02:13 +0300

    date

--- components/DateNavigator.tsx
+++ components/DateNavigator.tsx
@@ -40,7 +40,7 @@ export default function DateNavigator({ date }: Props) {
   };
 
   return (
-    <div className="max-w-3xl border-b bg-white text-black py-3 px-4 flex items-center justify-between gap-4">
+    <div className="max-w-3xl border-b bg-black text-white py-2 px-1 flex items-center justify-between gap-2">
 
       {/* LEFT: LIVE button */}
       <button className="bg-red-600 text-white px-3 py-1 text-sm rounded">
