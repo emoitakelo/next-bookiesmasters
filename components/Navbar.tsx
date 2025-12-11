9ec83eb4ff923c98aaa840166065bf925c3bfaeb
emoit akelo <emoitakelo@gmail.com>
2025-12-11 15:33:10 +0300

    date

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
           <Image
             src={logo}
             alt="BookiesMasters Logo"
-            className="h-40 w-34 object-contain"
+            className="h-40 w-30 object-contain"
             priority
           />
         </Link>
