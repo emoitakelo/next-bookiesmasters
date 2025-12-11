00d2915be90fea258b456369736043b62a37554d
emoit akelo <emoitakelo@gmail.com>
2025-12-11 14:26:59 +0300

    date

--- components/DateNavigator.tsx
+++ components/DateNavigator.tsx
@@ -45,7 +45,7 @@ export default function DateNavigator({ date }: Props) {
   };
 
   return (
-    <div className="max-w-3xl border-b bg-black text-white py-2 px-3 flex items-center justify-between gap-2">
+    <div className="max-w-3xl border-b bg-[#1F1F1F] text-white py-2 px-3 flex items-center justify-between gap-2">
 
       {/* LEFT: LIVE button (circular) */}
       <button className=" bg-gray-700 w-8 h-8 rounded-md flex items-center justify-center text-xs ">
@@ -76,7 +76,7 @@ export default function DateNavigator({ date }: Props) {
       {/* RIGHT: Circular calendar showing only the day number */}
       <button
         onClick={() => hiddenCalendar.current?.showPicker?.()}
-        className="w-8 h-8 bg-gray-700 text-white text-xs rounded-md border-white flex items-center justify-center "
+        className="w-8 h-8 bg-[#1F1F1F] text-white text-xs rounded-md border-white flex items-center justify-center "
       >
         {dayOnly}
       </button>
