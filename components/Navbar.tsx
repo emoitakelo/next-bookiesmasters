a77247933c217b9e8b596cd90d350a9c686172de
emoit akelo <emoitakelo@gmail.com>
2025-12-11 15:40:30 +0300

    date

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,13 +16,13 @@ export default function Navbar() {
 
   return (
     <nav className="w-full bg-[#1F1F1F] text-white shadow-md">
-      <div className="max-w-7xl mx-auto px-2 md:px-8 flex items-center justify-between h-15">
+      <div className="max-w-7xl mx-auto px-1 md:px-8 flex items-center justify-between h-15">
         {/* Logo */}
         <Link href="/" className="flex items-center gap-2">
           <Image
             src={logo}
             alt="BookiesMasters Logo"
-            className="h-52 w-28 object-contain"
+            className=" w-28 object-contain"
             priority
           />
         </Link>
