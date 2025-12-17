6c0f83d6aadd53020d7220e0750c395ed6ed8638
emoit akelo <emoitakelo@gmail.com>
2025-12-17 15:47:20 +0300

    details

--- app/predictions/[date]/loading.tsx
+++ app/predictions/[date]/loading.tsx
@@ -2,10 +2,9 @@ export default function Loading() {
     return (
         <div className="flex flex-col items-center justify-center min-h-[50vh] space-y-4">
             <div className="relative">
-                {/* Outer Ring */}
-                <div className="w-6 h-6 rounded-full border-4 border-[#1F1F1F]"></div>
+
                 {/* Inner Spinning Ring */}
-                <div className="absolute top-0 left-0 w-6 h-6 rounded-full border-4 border-t-white border-transparent animate-spin"></div>
+                <div className="absolute top-0 left-0 w-10 h-10 rounded-full border-4 border-t-white border-transparent animate-spin"></div>
             </div>
 
         </div>
